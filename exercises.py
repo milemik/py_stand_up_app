4831a19174dee0d7f58790fdeec75cd7faeedb7b
Ivan <ivanm@ernieapp.com>
2020-11-24 01:49:34 +0100

    Added more exercises

--- exercises.py
+++ exercises.py
@@ -1,18 +1,28 @@
 WORMUP = (
 	"10 SitUps",
-	"15 PushUps",
 	"10 PushUps",
 	"30 Side SitUps",
 	"3min run in place",
+	"3min jumping",
+	"2min move in 4 directions with situp",
 	)
 
 MID = (
 	"3min shadow box",
 	"1min plank",
+	"20 PushUps",
+	"1min PushUps",
+	"20 Leg Kiks",
+	"10 Biceps curls",
+	"10 Triceps curls",
+	"10 Shoulder pushups",
+	"10 Back straingth",
 )
 
 END = (
 	"TaiChi dancing witht he clouds",
-	"Chi kung some here",
-	"Give me more",
+	"Chi kung - hug the three",
+	"Sanchin kata",
+	"Siu Lim Tao",
+	"Un Push - Tai Chi",
 )
